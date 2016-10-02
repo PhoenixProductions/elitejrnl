64b937ff780828dfa1127a66c76e159442186ec3
Michael Hughes <michael@phoenixproductions.org.uk>
2016-10-02 21:05:03 +0100

    Responses

--- Bard/Responses/EventResponse.cs
+++ Bard/Responses/EventResponse.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Security.Cryptography;
+using System.Collections;
 namespace Bard
 {
 	/// <summary>
@@ -6,8 +8,57 @@ namespace Bard
 	/// </summary>
 	public class EventResponse
 	{
-		public EventResponse()
+		private string eventId;
+
+		private double signficance;
+
+		private System.Collections.Hashtable responses;
+		public EventResponse(string eventId) : this(eventId, 0.0)
 		{
+			this.eventId = eventId.ToLower();
+
+			this.responses = new System.Collections.Hashtable();
+		}
+		public EventResponse(string eventId, double signficance)
+		{
+			this.signficance = signficance;
+		}
+
+		public void AddResponse(string responseText)
+		{
+			string hash = EventResponse.GetMd5Hash(responseText);
+			if (!responses.ContainsKey(hash))
+			{
+				EventResponseClause rc = new EventResponseClause(responseText);
+				this.responses.Add(hash, rc);
+			}
+		}
+		/// <summary>
+		/// Select a random response clause
+		/// </summary>
+		public EventResponseClause Select()
+		{
+			int limit = this.responses.Count;
+			ArrayList keys = new System.Collections.ArrayList(responses.Keys);
+			Random r = new Random();
+			int ri = r.Next();
+			string k = (string)keys[ri];
+
+			return (EventResponseClause)this.responses[k];
+
+		}
+		protected static string GetMd5Hash(string input)
+		{
+			using (MD5 hash = MD5.Create())
+			{
+				byte[] data = hash.ComputeHash(System.Text.Encoding.UTF8.GetBytes(input));
+				System.Text.StringBuilder sb = new System.Text.StringBuilder();
+				for (int i = 0; i < data.Length; i++)
+				{
+					sb.Append(data[i].ToString("x2"));
+				}
+				return sb.ToString();
+			}
 		}
 	}
 }
---
+++ Bard/Responses/EventResponseClause.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace Bard
+{
+	/// <summary>
+	/// A single response phrase that can be made for a given event
+	/// </summary>
+	public class EventResponseClause
+	{
+		/// <summary>
+		/// Number of times this clause has been used
+		/// </summary>
+		private int usageCount = 0;
+
+		/// <summary>
+		/// When the clause was last used.
+		/// </summary>
+		private DateTime lastUsed;
+
+		private string text;
+		public EventResponseClause(string Text)
+		{
+			this.text = Text;
+			this.usageCount = 0;
+			this.lastUsed = DateTime.MinValue;
+		}
+	}
+}
