4a6cab39183e8c581086f93c5c7b19b7f59587c8
Michael Hughes <michael@phoenixproductions.org.uk>
2016-10-02 20:28:00 +0100

    Added Missing pilot EventARgs

---
+++ elitecore/PilotEvents/PilotEventArgs.cs
@@ -0,0 +1,47 @@
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+
+namespace elitecore
+{
+	public class PilotEventArgs : EventArgs
+	{
+		public string rawEventLog;
+
+		public DateTime Timestamp;
+
+		public string Event;
+
+		public Dictionary<string, string> data;
+
+		public PilotEventArgs(string LogData)
+		{
+			this.data = new Dictionary<string, string>();
+			this.rawEventLog = LogData;
+			JsonTextReader tr = new JsonTextReader(new StringReader(LogData));
+			string lastPropName = "";
+			while (tr.Read())
+			{
+				bool flag = tr.TokenType == JsonToken.String && lastPropName != "";
+				if (flag)
+				{
+					Debug.WriteLine("Got string value and propname");
+					this.data.Add(lastPropName, tr.Value.ToString());
+					lastPropName = "";
+				}
+				else
+				{
+					bool flag2 = tr.TokenType == JsonToken.PropertyName;
+					if (flag2)
+					{
+						Debug.WriteLine("Setting propname");
+						lastPropName = tr.Value.ToString();
+					}
+				}
+			}
+		}
+	}
+}
+""
