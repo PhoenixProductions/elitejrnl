849b9e97a54b4c5896ec86bf510a2daceb48e89a
Michael Hughes <michael@phoenixproductions.org.uk>
2016-10-03 13:36:16 +0100

    stuff

---
+++ Bard/PlotEvents/Navigation/Docking.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Bard
+{
+	[Serializable]
+	public class Docking : PlotEvent
+	{
+		public Docking()
+		{
+		}
+
+		public bool CanHandle(string eventId)
+		{
+			switch(eventId.ToLower()) {
+				case "docked":
+				case "undocked":
+					return true;
+				default:
+					return false;
+		}
+	}
+}
